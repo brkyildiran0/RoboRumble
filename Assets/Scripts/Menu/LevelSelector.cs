802ff2b7dd6a40ac21eca13d780384eb83a363e3
Alperen Erdoğan <alperenerdogan@yahoo.com>
2024-03-06 19:52:01 +0300

    Level selection

---
+++ Assets/Scripts/Menu/LevelSelector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    public Text textLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textLevel.text = level.ToString();
+        
+    }
+
+    public void OpenScene(){
+        SceneManager.LoadScene("Level "+level.ToString());
+    }
+}
