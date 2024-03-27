af61248621561b144efd49500100ef3712ca3ac5
Alperen Erdoğan <alperenerdogan@yahoo.com>
2024-03-27 17:37:35 +0300

    MainMenu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GoSelecLevel (){
+        SceneManager.LoadScene("LevelSelection");
+    }
+    public void GoSettings (){
+        SceneManager.LoadScene("Settings");
+    }
+    public void GoQuit (){
+        Application.Quit();
+    }
+}
